04e301f6358065665e187bc69e3647c34c8e37b6
Rura_M <61036634+Rura-M@users.noreply.github.com>
2023-02-27 13:37:13 -0600

    Add files via upload

---
+++ week1/week1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by Ruramaimunashe Mutefura on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func didTapLabel(_ sender: UITapGestureRecognizer) {
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "detailSegue", sender: tappedView)
+        }
+    }
+    
+}
+
